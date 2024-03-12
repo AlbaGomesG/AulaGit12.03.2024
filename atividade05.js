c9eefbbcc6311aff02122a2cd38f914abfcdbb00
AlbaGomesG <giovannaalba.tds1.senai@gmail.com>
2024-03-12 09:32:47 -0300

    mudei mensagem de entrada

--- atividade05.js
+++ atividade05.js
@@ -4,7 +4,7 @@ let numero = 20
 //fazer a condição: SE ou IF
 if(numero >= 20){
     //exiba bem-vindo
-    console.log("bem-vindo");
+    console.log("seja bem-vindo");
 }else{
     //exiba tente novamente
     console.log("tente novamente");
