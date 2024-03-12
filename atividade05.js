088617cdb9da59de977490c7e36cfcf553ed6293
AlbaGomesG <giovannaalba.tds1.senai@gmail.com>
2024-03-12 10:51:53 -0300

    exiba liberado

--- atividade05.js
+++ atividade05.js
@@ -3,8 +3,8 @@ let numero = 18
 
 //fazer a condição: SE ou IF
 if(numero >= 18){
-    //exiba acesso liberado
-    console.log("acesso liberado");
+    //exiba liberado
+    console.log("liberado");
 }else{
     //exiba erro, tente novamente
     console.log("erro, tente novamente");
