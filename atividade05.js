6d69a33c833f45c16c210c04fecf0d51d51fe45d
AlbaGomesG <giovannaalba.tds1.senai@gmail.com>
2024-03-12 09:25:58 -0300

    let = 16

--- atividade05.js
+++ atividade05.js
@@ -1,8 +1,8 @@
 //iniciar o numero
-let numero = 10
+let numero = 16
 
 //fazer a condição: SE ou IF
-if(numero >= 10){
+if(numero >= 16){
     //exiba acesso permitido
     console.log("Acesso permitido");
 }else{
