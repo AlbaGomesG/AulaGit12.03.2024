c07221f4426d7ac7e1eb428e2e470681652468ab
AlbaGomesG <giovannaalba.tds1.senai@gmail.com>
2024-03-12 08:35:10 -0300

    primeiro commit

---
+++ condi.js
@@ -0,0 +1,12 @@
+//Iniciar numero
+let numero = 0
+//Fazer condição: SE ou IF
+if(numero > 0){
+    //exiba numero positivo
+    console.log("Esse numero é positivo");
+}else if(numero < 0){
+    //exiba numero negativo
+    console.log("Esse numero é negativo");
+}else{
+    console.log("O numero é 0");
+}
