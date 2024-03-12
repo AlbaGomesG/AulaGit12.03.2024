28b997f7a7abdbe20355b188e51b41a56f51ac22
AlbaGomesG <giovannaalba.tds1.senai@gmail.com>
2024-03-12 10:45:20 -0300

    mudei a mensagem de acesso

--- atividade05.js
+++ atividade05.js
@@ -3,8 +3,8 @@ let numero = 20
 
 //fazer a condição: SE ou IF
 if(numero >= 20){
-    //exiba bem-vindo
-    console.log("seja bem-vindo");
+    //exiba acesso liberado
+    console.log("acesso liberado");
 }else{
     //exiba tente novamente
     console.log("acesso negado");
