e34a17da0f39dae0665f850e3c678c831b47fe1e
AlbaGomesG <giovannaalba.tds1.senai@gmail.com>
2024-03-12 08:54:18 -0300

    first commit

---
+++ atividade05.js
@@ -0,0 +1,11 @@
+//iniciar o numero
+let numero = 18 
+
+//fazer a condição: SE ou IF
+if(numero >= 18){
+    //exiba acesso permitido
+    console.log("Acesso permitido");
+}else{
+    //exiba acesso negado
+    console.log("Acesso negado");
+}
